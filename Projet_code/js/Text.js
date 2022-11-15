c931bd9654d5f31928356058a51ddca5e1fd6676
Relhoq <quentin@kokofamily.ch>
2022-11-15 16:02:47 +0100

    text center

--- Projet_code/js/Text.js
+++ Projet_code/js/Text.js
@@ -8,11 +8,10 @@ class Text {
   drawText(x, y) {
     this.ctx.fillStyle = "black";
     this.ctx.strokeStyle = "black";
-
     //this.ctx.strokeRect(this.x, this.y, this.x1 / 3, this.y1 / 3);
     this.ctx.font = `` + this.size + `px Garaje`;
     this.ctx.textAlign = "center";
-    this.ctx.textBaseline = "bottom";
+    this.ctx.textBaseline = "middle";
 
     this.ctx.fillText(this.text, x, y);
   }
