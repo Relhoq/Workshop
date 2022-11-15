a97ea39ced8b31e4f17ec42468e09eb417e5b2cc
Relhoq <quentin@kokofamily.ch>
2022-11-15 15:28:16 +0100

    work

---
+++ Projet_code/js/App.js
@@ -0,0 +1,33 @@
+let size = 300;
+
+class App {
+  constructor() {
+    this.pixelRatio = window.devicePixelRatio || 1;
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = window.innerWidth * this.pixelRatio;
+    this.canvas.height = window.innerHeight * this.pixelRatio;
+    this.canvas.style.width = window.innerWidth;
+    this.canvas.style.height = window.innerHeight;
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d");
+    this.setup();
+  }
+
+  setup() {
+    let x = window.innerWidth / 2;
+    let y = window.innerHeight / 2;
+
+    const texte = new Text(this.ctx, "IS CODING FUN?", 100);
+
+    this.ctx.save();
+    this.ctx.translate(x, y);
+    this.ctx.rotate(2 * Math.PI);
+    texte.drawText(200 / 2, 200 / 2);
+
+    this.ctx.restore();
+  }
+}
+
+window.onload = function () {
+  new App();
+};
---
+++ Projet_code/js/Text.js
@@ -0,0 +1,19 @@
+class Text {
+  constructor(ctx, text, size) {
+    this.text = text;
+    this.ctx = ctx;
+    this.size = size;
+  }
+
+  drawText(x, y) {
+    this.ctx.fillStyle = "black";
+    this.ctx.strokeStyle = "black";
+
+    //this.ctx.strokeRect(this.x, this.y, this.x1 / 3, this.y1 / 3);
+    this.ctx.font = `` + this.size + `px Garaje`;
+    this.ctx.textAlign = "center";
+    this.ctx.textBaseline = "bottom";
+
+    this.ctx.fillText(this.text, x, y);
+  }
+}
